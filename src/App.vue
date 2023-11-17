e63a34bef8e0948c88c7343893f958191d748d14
Thiagodfarias <thiago__07@hotmail.com>
2023-11-17 11:13:03 -0400

    commit para estilização

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,108 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app" class="container mt-4">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 calculator">
+        <header>
+          <h1 class="header-text">
+            Calculadora
+          </h1>
+        </header>
+        <label for="numero1" class="label">NÚMERO 1:</label>
+        <input v-model.number="numero1" type="number" class="input text-black">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <label for="numero2" class="label">NÚMERO 2:</label>
+        <input v-model.number="numero2" type="number" class="input text-black">
+
+        <label for="operacao" class="label">OPERAÇÃO:</label>
+        <select v-model="operacao" class="select text-black">
+          <option value="+" class="option">+ SOMAR</option>
+          <option value="-" class="option">- SUBTRAIR</option>
+          <option value="*" class="option">X MULTIPLICAR</option>
+          <option value="/" class="option">÷ DIVIDIR</option>
+        </select>
+
+        <div class="result">
+          <strong>RESULTADO:</strong> {{ calcularResultado }}
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      numero1: '0',
+      numero2: '0',
+      operacao: '+',
+    };
+  },
+  computed: {
+    calcularResultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.numero1 + this.numero2;
+        case '-':
+          return this.numero1 - this.numero2;
+        case '*':
+          return this.numero1 * this.numero2;
+        case '/':
+          return this.numero1 / this.numero2;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
+
 <style scoped>
+.calculator {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 15px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #000;
+}
+
 header {
-  line-height: 1.5;
+  text-align: center;
+  margin-bottom: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header-text {
+  color: #fff;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.label,
+.input,
+.select,
+.result {
+  color: #fff;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.text-black {
+  color: black;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.input,
+.select {
+  width: 100%;
+  padding: 8px;
+  margin: 6px 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #eceff1;
+}
+
+.select {
+  appearance: none;
+  background-color: orange;
+}
+
+.option:checked {
+  background-color: grey;
+  color: #fff;
 }
 </style>
